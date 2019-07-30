db898c1e273c2964d454f1d649776c5766c5f14d
Gigi M <mmgamao1@gmail.com>
2019-07-30 17:54:15 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, and the winner of the contest is...."
+    resp.write "Hello, my name is"
     resp.finish
   end
 
